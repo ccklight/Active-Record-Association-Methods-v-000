37a1e038a4ecb0468aa4554d6cbeffa29a89658d
Cheryl K <cherylkelson@icloud.com>
2019-01-28 22:32:28 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -12,14 +12,6 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    # return an array of strings containing every musician's name
-  end
-end
-
-
-
-  def all_artist_names
-    # return an array of strings containing every musician's name
     self.artists.collect {|artist| artist.name}
   end
 end
