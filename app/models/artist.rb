22369456fee7c63bcb689341cf8d5559e9cbe4cd
Cheryl K <cherylkelson@icloud.com>
2019-01-28 22:16:46 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,13 +1,13 @@
-require 'pry' 
+require 'pry'
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    self.song.genre.first 
+    self.song.genre.first
       binding pry
     #return the genre of the artist's first saved song
-  
+
   end
 
   def song_count
